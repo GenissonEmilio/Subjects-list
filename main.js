513ec0d6327452f3777afe8b6a77c5dc2970cccb
GenissonEmilio <esgenisson@gmail.com>
2024-07-18 15:35:57 -0300

    update

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Array com os IDs dos checkboxes
+  const checkboxes = ["as1", "as2", "as3", "as4", "as5", "as6", "as7", "as8", "as9", "as10", "as11"];
+
+  // Carregar o estado dos checkboxes do localStorage
+  checkboxes.forEach(function(id) {
+    const checkbox = document.getElementById(id);
+    const checked = localStorage.getItem(id) === "true";
+    checkbox.checked = checked;
+  });
+
+  // Salvar o estado dos checkboxes no localStorage
+  checkboxes.forEach(function(id) {
+    const checkbox = document.getElementById(id);
+    checkbox.addEventListener("change", function() {
+      localStorage.setItem(id, checkbox.checked);
+    });
+  });
+});
